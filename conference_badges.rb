fe9b2ca31a4ca0c1997a0c908775b0cf527e97f9
Adam Spencer <aspencer0919@gmail.com>
2020-03-31 20:32:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(badges)
-  badges.each do |attendees|
-    return badge_maker(attendees)
+def batch_badge_creator(names)
+  greetings = []
+  names.each do |name|
+    greetings <<
   end
+  return greetings
 end
 
 def assign_rooms(speakers, rooms)
