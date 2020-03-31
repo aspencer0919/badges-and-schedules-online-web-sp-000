ff6358dc3d6b5392bc67bb581d0b06238d85b842
Adam Spencer <aspencer0919@gmail.com>
2020-03-31 20:38:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 
 def assign_rooms(speakers)
   greet = []
-  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index}!"}
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"}
   return greet
 end
 
